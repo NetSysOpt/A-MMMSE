0369f7d631e571d07c852f39f4e5f4dff781a99a
Xi Gao <57949104+xxxxi-17@users.noreply.github.com>
2025-10-23 17:17:09 +0800

    Update MIMO_GPU.sh

--- scripts/MIMO_GPU.sh
+++ scripts/MIMO_GPU.sh
@@ -10,7 +10,7 @@ python main_gpu.py --config .\configs\MIMO.yaml --model_name A_MMMSE_GPU --T 512
 
 python main_gpu.py --config .\configs\MIMO.yaml --model_name R_WMMSE_GPU --T 1024 --I 20 --d 4 --snr 10
 python main_gpu.py --config .\configs\MIMO.yaml --model_name A_MMMSE_GPU --T 1024 --I 20 --d 4 --snr 10 --omega 0.4 --lr 0.1
-v
+
 python main_gpu.py --config .\configs\MIMO.yaml --model_name R_WMMSE_GPU --T 2048 --I 20 --d 4 --snr 10
 python main_gpu.py --config .\configs\MIMO.yaml --model_name A_MMMSE_GPU --T 2048 --I 20 --d 4 --snr 10 --omega 0.4 --lr 0.1
 
